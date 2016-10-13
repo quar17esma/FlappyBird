a7561f353f017860a98bd9898e12f985fec7f198
quar17esma <quar17esma@gmail.com>
2016-10-13 22:29:18 +0300

    comment and refactor, add preGameRoot

--- src/main/java/PreGameRoot.java
+++ src/main/java/PreGameRoot.java
@@ -7,15 +7,19 @@ public class PreGameRoot extends Pane {
     //подсказка (клик по окну)
     private Tap tap;
 
+    //координаты елементов
+    private static final double GET_READY_Y = 130;
+    private static final double TAP_Y = 250;
+
     //создаем и добавляем елементы
     public PreGameRoot(Bird bird) {
         getReady = new GetReady();
         getReady.setTranslateX(bird.velocity.getX()+(Game.getStageWidth()-GetReady.getFitWidth())/2);
-        getReady.setTranslateY(130);
+        getReady.setTranslateY(GET_READY_Y);
 
         tap = new Tap();
         tap.setTranslateX(bird.velocity.getX()+(Game.getStageWidth()-Tap.getFitWidth())/2);
-        tap.setTranslateY(250);
+        tap.setTranslateY(TAP_Y);
 
         getChildren().addAll(getReady, tap);
     }
